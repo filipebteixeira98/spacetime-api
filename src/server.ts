9f67eaf2882cbfe96aa447914f771ebfee589ccf
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-16 15:41:40 -0300

    feat(server): enable CORS support

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
 
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.get('/health', () => {
   return 'OK'
